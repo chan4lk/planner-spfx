e9e9a98eb421cd6e1a87ce9b1a861f62c6d2da11
Chandima Ranaaweera <chan4lk@gmail.com>
2017-03-01 21:42:51 +0530

    New files added

---
+++ src/webparts/pannerWebPart/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IPannerWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'pannerWebPartStrings' {
+  const strings: IPannerWebPartStrings;
+  export = strings;
+}
